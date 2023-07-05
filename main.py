15f914426408669c543aa97d9225b9d138bfbcda
varvara-chornomorets <vchornomorets@kse.org.ua>
2023-07-05 14:15:32 +0300

    generate some csv -graphs

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+import csv
+import os
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+nodes_list = ['A', 'B', 'C', 'D', 'E']  # List of nodes
+num_files = 5  # Number of CSV files to create
+directory = 'graphs'  # Directory name
 
+# Create the directory if it doesn't exist
+if not os.path.exists(directory):
+    os.makedirs(directory)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+for i in range(num_files):
+    filename = f"file_{i+1}.csv"  # Construct the filename for each CSV file
+    filepath = os.path.join(directory, filename)  # Create the full filepath
 
+    with open(filepath, 'w', newline='') as file:
+        writer = csv.writer(file)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        # Write the header row
+        writer.writerow(['node1', 'node2', 'trail', 'distance'])
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        # Generate random data rows
+        for j in range(10):
+            node1 = random.choice(nodes_list)
+            node2 = random.choice(nodes_list)
+            trail = chr(97 + j)  # Convert 0-9 to 'a'-'j'
+            distance = random.randint(1, 10)  # Random distance between 1 and 10
+
+            writer.writerow([node1, node2, trail, distance])
+
+    print(f"CSV file '{filename}' created successfully.")
