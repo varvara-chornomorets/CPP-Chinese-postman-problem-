4cee94e3c40e31122ebe2cb2fd4311e840c9bd5b
varvara-chornomorets <vchornomorets@kse.org.ua>
2023-07-11 16:33:56 +0300

    changes in structure to make it more clear

--- main.py
+++ main.py
@@ -79,74 +79,29 @@ def generate_graphs(num_vertices, num_graphs):
         print(f"CSV file '{filename}' is connected with {edges_added} edges.")
 
 
-def readGraphs():
-    parent_directory = 'graphs'  # parent directory name
+def findCPP(graph, costs):
+    for vertex, edges in graph.items():
+        print(vertex, "->", edges)
+    odd_vertices = find_odd_vertices(graph)
+    if (odd_vertices):
+        # 1.find odds
+        # 2. pair them somehow (add new dublicating edges) and see what's going to be cheaper
+        # return is only for now, later i'll remove it
+        print("ODD VERTICES")
+        return
 
-    # iterate over all directories
-    for directory in os.listdir(parent_directory):
-        directory_path = os.path.join(parent_directory, directory)  # Full directory path
-
-        # check if the item is a directory
-        if os.path.isdir(directory_path):
-            print(f"STARTING NEW DIRECTORY{directory}")
-            # iterate over files in the directory
-            for filename in os.listdir(directory_path):
-                if filename.endswith('.csv'):  # check if the file is a csv
-                    file_path = os.path.join(directory_path, filename)  # full file path
-
-                    # do everything we need
-                    # THIS SHOULD WORK ONLY IF GRAPHS ARE EULErIAN SO WE WILL CHANge IT
-                    print(f"Processing file '{filename}' in directory '{directory}':")
-                    try:
-                        graph, costs = read_graph_from_csv(file_path)
-
-                        # for vertex, edges in graph.items():
-                        #     print(vertex, "->", edges)
-
-                        eulerian_cycle, cost = find_eulerian_cycle(graph, costs)
-                        print("Eulerian Cycle:", "->".join(eulerian_cycle))
-                    except Exception:
-                        print("graph is not eulerian i guess")
-                        pass
-                    print()
+    eulerian_cycle, cost = find_eulerian_cycle(graph, costs)
+    print("Eulerian Cycle:", "->".join(eulerian_cycle))
 
     return
-
-
-def find_vertices_with_odd_degree(csv_file):
-    degrees = {}
-
-    # Read the CSV file and count the occurrences of each vertex
-    with open(csv_file, 'r') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            node1 = row['node1']
-            node2 = row['node2']
-
-            if node1 in degrees:
-                degrees[node1] += 1
-            else:
-                degrees[node1] = 1
-
-            if node2 in degrees:
-                degrees[node2] += 1
-            else:
-                degrees[node2] = 1
-
-    # Find vertices with odd degree
-    odd_vertices = [vertex for vertex, degree in degrees.items() if degree % 2 == 1]
+def find_odd_vertices(graph: dict) -> list:
+    odd_vertices = []
+    for vertex, connected_vertices in graph.items():
+        if len(connected_vertices) % 2 == 1:
+            odd_vertices.append(vertex)
     return odd_vertices
 
 
-def findCPP():
-    # if all vertices are even
-    #      find euliarian cycle
-    # if not all vertices are even
-    #          1. find odds
-    #          2. pair them somehow (add new dublicating edges) and see what's going to be cheaper
-    #          3. find eulerian cycle for new graph
-
-    return 5
 
 
 def read_graph_from_csv(file_path):
@@ -195,6 +150,38 @@ def find_eulerian_cycle(graph, costs):
         cost += costs[(cycle[i], cycle[i+1])]
 
     return cycle, cost
+
+
+
+def readGraphs():
+    parent_directory = 'graphs'  # parent directory name
+
+    # iterate over all directories
+    for directory in os.listdir(parent_directory):
+        directory_path = os.path.join(parent_directory, directory)  # Full directory path
+
+        # check if the item is a directory
+        if os.path.isdir(directory_path):
+            print(f"STARTING NEW DIRECTORY{directory}")
+            # iterate over files in the directory
+            for filename in os.listdir(directory_path):
+                if filename.endswith('.csv'):  # check if the file is a csv
+                    file_path = os.path.join(directory_path, filename)  # full file path
+
+                    # do everything we need
+                    # THIS SHOULD WORK ONLY IF GRAPHS ARE EULErIAN SO WE WILL CHANge IT
+                    print(f"Processing file '{filename}' in directory '{directory}':")
+                    try:
+                        graph, costs = read_graph_from_csv(file_path)
+                        findCPP(graph, costs)
+                    except Exception:
+                        print("graph is not eulerian i guess")
+                        pass
+                    print()
+
+    return
+
+
 
 
 readGraphs()
